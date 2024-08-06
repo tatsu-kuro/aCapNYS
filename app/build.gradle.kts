5c414246e694fd1e984fdfdcbfba5f85124df7ff
tatsu <tatsuakikuroda@me.com>
2024-08-06 11:21:29 +0900

    permission.READ_MEDIA_VIDEOを削除。 自分で録画したものしか削除できなくなったが、非常にシンプルになった。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.kuroda33.acapnys"
         minSdk = 24
         targetSdk = 34
-        versionCode = 34
+        versionCode = 35
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
